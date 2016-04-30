e11db4d0c54296de8909884bf9c7bc7e49e3f942
Connor Bradford <blazezexal@gmail.com>
2016-04-30 10:25:36 -0500

    added ability to make game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,10 @@ class GamesController < ApplicationController
 	def index
 		@games = Game.all
 	end
+	def new
+		@game = Game.new
+	end
+	def create
+
+	end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,6 @@
 class Game < ActiveRecord::Base
   attr_accessible :name, :rating, :thoughts
+  def test_method (var)
+    puts "hola #{var}"
+  end
 end
